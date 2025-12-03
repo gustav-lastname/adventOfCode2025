1d6319de30441814656521bd4545b180d63a0196
Gustav Holmberg <gustav.lastname@gmail.com>
2025-12-03 10:07:21 +0100

    Solve day 3 part 1

---
+++ dag3/dag3.lua
@@ -0,0 +1,29 @@
+local input = {}
+
+for line in io.lines('dag3/input.txt') do table.insert(input, line) end
+
+local joltsum = 0
+
+for _, bank in ipairs(input) do
+    local firstposition = 1
+    local firstvalue = tonumber(bank:sub(1,1))
+    for i = 2, #bank-1 do
+        if tonumber(bank:sub(i,i)) > firstvalue then
+            firstposition = i
+            firstvalue = tonumber(bank:sub(i,i))
+        end
+    end
+
+    local secondposition = firstposition + 1
+    local secondvalue = tonumber(bank:sub(secondposition,secondposition))
+    for i = secondposition + 1, #bank do
+        if tonumber(bank:sub(i,i)) > secondvalue then
+            secondposition = i
+            secondvalue = tonumber(bank:sub(i,i))
+        end
+    end
+
+    local bankjolt = firstvalue..secondvalue
+    joltsum = joltsum + bankjolt
+end
+print(joltsum)
