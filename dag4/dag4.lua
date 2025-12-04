29766cef5414ee75308d0fff9584720ee8789abe
Gustav Holmberg <gustav.lastname@gmail.com>
2025-12-04 09:58:57 +0100

    Solve day 4 part 1

---
+++ dag4/dag4.lua
@@ -0,0 +1,37 @@
+local input = {}
+
+local available_paper = 0
+
+for line in io.lines('input.txt') do
+    table.insert(input, line)
+end
+
+local function ispaper(row_nr, col_nr)
+    if input[row_nr] == nil then return false end
+    if col_nr < 1 or row_nr < 1 or col_nr > #input[row_nr] then
+        return false
+    end
+    if input[row_nr]:sub(col_nr,col_nr) == '@' then
+        return true
+    else
+        return false
+    end
+end
+
+for row_nr, row in ipairs(input) do
+    for col_nr = 1, #row do
+        if ispaper(row_nr,col_nr) then
+            local adjacent_paper = 0
+            for col_ofset = -1,1 do
+                for row_ofset = -1,1 do
+                    adjacent_paper = adjacent_paper + (ispaper(row_nr + row_ofset, col_nr + col_ofset) and 1 or 0)
+                end
+            end
+            if adjacent_paper < 5 then
+                available_paper = available_paper + 1
+            end
+        end
+    end
+end
+
+print(available_paper)
